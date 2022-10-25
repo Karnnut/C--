6345ea6cdbf05b9951cd887702cf1dc08587dd62
Karnnut <112846988+Karnnut@users.noreply.github.com>
2022-10-25 10:58:22 +0700

    commit

--- Final_Cpp_Lab/HW_10/HW10.cpp
+++ Final_Cpp_Lab/HW_10/HW10.cpp
@@ -1,38 +1,90 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-/*
-     WRITE YOUR CODE HERE
-*/
+#include <queue>
+#include <algorithm>
+
 using namespace std;
-class Village {
-  int numHouse;
-  /*
-       WRITE YOUR CODE HERE
-  */
+class Village{
+    int numHouse;
+    vector<int> *adjLists;
+    bool *bfs;
+    bool *dfs;
 
 public:
-  Village(int V) {
-    /*
-         WRITE YOUR CODE HERE
-    */
-  }
-  void addRoad(int src, int dest) {
-    /*
-         WRITE YOUR CODE HERE
-    */
-  }
-  void BFS(int firstHouse) {
-    /*
-         WRITE YOUR CODE HERE
-    */
-  }
-  void DFS(int firstHouse) {
-    /*
-         WRITE YOUR CODE HERE
-    */
-  }
-  /*
-       WRITE YOUR CODE HERE
-  */
+    Village(int V){
+      numHouse = V;
+      adjLists = new vector<int>[V];
+    }
+    void addRoad(int src, int dest){
+      adjLists[src].push_back(dest);
+      adjLists[dest].push_back(src);
+    }
+
+    void sort_adjLists(){
+      for(int i = 0; i < numHouse; i++){
+        sort(adjLists[i].begin(),adjLists[i].end());
+      }
+    }
+    
+     void DFS(int firstHouse){
+      dfs = new bool[numHouse];
+      for (int i = 0; i < numHouse; i++)
+        dfs[i] = false;
+      
+      DFSVISIT(firstHouse);
+      
+      for(int i=0;i<numHouse;i++){
+        if(dfs[i]==false){
+          DFSVISIT(i);
+        }
+      }  
+    }
+   
+    void BFS(int firstHouse){
+      bfs = new bool[numHouse];
+      for (int i = 0; i < numHouse; i++)
+        bfs[i] = false;
+      queue<int> q;
+      q.push(firstHouse);
+      while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        if(bfs[u])
+          continue;
+        bfs[u] = true;
+        cout<<u<<" ";
+        sort_adjLists();
+        vector<int>::iterator it;
+        for(it = adjLists[u].begin(); it!=adjLists[u].end(); it++){
+          if(!bfs[*it])
+            q.push(*it);
+        }
+      }
+    }
+    void DFSVISIT(int vertex){
+      dfs[vertex] = true;
+      cout<< vertex<< " ";
+      sort_adjLists();
+      vector<int>::iterator it;
+      for(it = adjLists[vertex].begin(); it!=adjLists[vertex].end(); it++){
+        if(!dfs[*it])
+          DFSVISIT(*it);
+      }
+    }
+   
+    void printGraph(){
+      for(int i=0; i<numHouse; i++){
+      vector<int> adjList = adjLists[i];
+      vector<int>::iterator it;
+      cout<<i<<"->";
+      for(it = adjList.begin(); it!=adjList.end(); it++){
+        cout<<*it<<" ";
+      }
+      cout<<"\n";
+      }
+    }
 };
+
+
+
+
